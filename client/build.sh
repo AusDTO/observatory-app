833f97515f8b594274a481f86f5ab63c214494b0
Raj <sukhrajghuman@live.com>
2020-09-04 10:45:51 +1000

    Update build.sh

--- client/build.sh
+++ client/build.sh
@@ -2,6 +2,6 @@ STATIC_FILE="pushstate: enabled"
 
 echo $STATIC_FILE >>build/Staticfile
 
-AUTH='observatory:$2y$10$wVrImWAWwoLjq150IHdJTuVckFC6A5hBUJ4/oB5p9A6pdK6WWKzwu'
+# AUTH='observatory:$2y$10$wVrImWAWwoLjq150IHdJTuVckFC6A5hBUJ4/oB5p9A6pdK6WWKzwu'
 
-echo $AUTH >> build/Staticfile.auth
+# echo $AUTH >> build/Staticfile.auth
