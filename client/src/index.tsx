9618230632752a0cd114d28b655ff37877642f4b
Raj <sukhrajghuman@live.com>
2020-09-04 13:19:53 +1000

    Update index.tsx

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -12,7 +12,7 @@ const client = new ApolloClient({
       ? "http://localhost:4000/graphql"
       : "/graphql", //FIX needs to be conditional
   cache: new InMemoryCache(),
-  credentials: "same-origin", // FIX CORS
+  credentials: "include", // FIX CORS
 });
 
 ReactDOM.render(
