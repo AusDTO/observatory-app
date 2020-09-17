c2c08081456a2b76ca8ea008a628eaa2e95ee107
Raj <sukhrajghuman@live.com>
2020-09-17 11:40:26 +1000

    add dotenv

--- bq_scripts/constants.py
+++ bq_scripts/constants.py
@@ -1,1 +1,15 @@
 API_STAGING="https://observatory-app.apps.y.cld.gov.au/api"
+
+
+
+## Using environment variables
+from dotenv import load_dotenv
+load_dotenv()
+
+from pathlib import Path  # Python 3.6+ only
+env_path = Path('.') / '.env'
+load_dotenv(dotenv_path=env_path)
+import os
+
+API_USERNAME = os.getenv("USERNAME")
+API_PASSWORD = os.getenv("PASSWORD")
