3a4e22abb1b690b73c94c8fe4deac9b2735781cf
Raj <sukhrajghuman@live.com>
2020-09-17 15:14:26 +1000

    Update constants.py

--- bq_scripts/constants.py
+++ bq_scripts/constants.py
@@ -13,5 +13,6 @@ load_dotenv()
 env_path = Path('.') / '.env'
 load_dotenv(dotenv_path=env_path)
 
+# Make sure USERNAME and PASSWORD exist in .env file
 API_USERNAME = os.getenv("USERNAME")
 API_PASSWORD = os.getenv("PASSWORD")
