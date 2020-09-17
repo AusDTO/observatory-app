f19c486c6133da7db242b9541a0f5c8fb2b56614
Raj <sukhrajghuman@live.com>
2020-09-17 15:27:27 +1000

    tidy up code

--- bq_scripts/data_ops.py
+++ bq_scripts/data_ops.py
@@ -7,24 +7,28 @@ token = signin.ACCESS_TOKEN
 header_token = {'Authorization': 'Bearer ' + token}
 
 
-def add_agency():
-    data = [{
-        "name": "DTA Comms",
-        "emailHosts": ["@dta.gov.au", "@digital.gov.au"]
-    }]
+def add_agency(agencyData):
+    # data = [{
+    #     "name": "DTA Comms",
+    #     "emailHosts": ["@dta.gov.au", "@digital.gov.au"]
+    # }]
     response = requests.post(constants.AGENCIES_ENDPOINT,
-                             json=data, headers=header_token)
-    print(response.json())
-    response.raise_for_status()
+                             json=agencyData, headers=header_token)
+    responseData = response.json()
+    print(responseData)
 
 
 def get_agencyId_by_name(agency_name):
     response = requests.get(
         f'{constants.AGENCIES_ENDPOINT}/{agency_name}', headers=header_token)
-    print(response.json())
     responseData = response.json()
-    agencyId = responseData.id
-    return agencyId
+    print(responseData)
+    if(response.status != 200):
+        # handle error, may want to do it another way
+        print("ERROR not found")
+    else:
+        agencyId = responseData.id
+        return agencyId
 
 
 def get_all_agencies():
@@ -32,21 +36,22 @@ def get_all_agencies():
                             headers=header_token)
     responseData = response.json()
     print(responseData)
-    return responseData
 
 
-def add_property():
-    data = [{"service_name": "Design system",
-             "domain": "www.designsystem.gov.au", "ua_id": "UA-1234", "agencyId": "12s2-213d-h8d9-hhj9"}]
+def add_property(propertyData):
+    # data = [{"service_name": "Design system",
+    #          "domain": "www.designsystem.gov.au", "ua_id": "UA-1234", "agencyId": "12s2-213d-h8d9-hhj9"}]
     response = requests.post(
-        constants.PROPERTIES_ENDPOINT, json=data, headers=header_token)
-    print(response.json())
+        constants.PROPERTIES_ENDPOINT, json=propertyData, headers=header_token)
+    responseData = response.json()
+    print(responseData)
 
 
 def get_all_properties():
     response = requests.get(constants.PROPERTIES_ENDPOINT,
                             headers=header_token)
-    print(response.json())
+    responseData = response.json()
+    print(responseData)
     return response.raise_for_status()
 
 
