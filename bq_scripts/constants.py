a9fb2a023e1e49ee95f24146600dc8e98b814d24
mufaddal <mufaddal.taiyab-ali@dta.gov.au>
2020-09-18 22:17:31 +1000

    constants of obs service framework

--- bq_scripts/constants.py
+++ bq_scripts/constants.py
@@ -16,3 +16,15 @@ load_dotenv(dotenv_path=env_path)
 # Make sure USERNAME and PASSWORD exist in .env file
 API_USERNAME = os.getenv("USERNAME")
 API_PASSWORD = os.getenv("PASSWORD")
+
+
+DAGS_DIR = '/home/airflow/gcs/dags/'
+if not os.path.isdir(DAGS_DIR):
+    DAGS_DIR = '../../dags/'
+
+
+# BigQuery Variables
+DATASET_ID = 'dta_customers'
+TABLE_ID = 'dta_ga_accounts'
+
+AGENCY_NAME = 'DTA'
