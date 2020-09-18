2079b1cde86dc40613e27983067730b063ecf707
mufaddal <mufaddal.taiyab-ali@dta.gov.au>
2020-09-18 22:59:00 +1000

    DTA agency data pipeline from BQ to RDS

--- bq_scripts/dag_bq_ingest_pipe_accounts_data.py
+++ bq_scripts/dag_bq_ingest_pipe_accounts_data.py
@@ -14,10 +14,6 @@ from airflow.operators import python_operator
 from airflow.contrib.operators import bigquery_operator
 from airflow.contrib.operators import bigquery_get_data
 
-from google.cloud import bigquery
-
-from galileo import galileo, searchconsole, ga
-
 import logging
 import json
 import requests
@@ -25,18 +21,20 @@ import re
 import six
 import logging
 from requests.exceptions import HTTPError
-# import pandas
-# import data_ops
-# import signin
+import env
+import signin
+import constants
+import data_ops
 
-# token = signin.ACCESS_TOKEN
+token = signin.ACCESS_TOKEN
+header_token = {'Authorization': 'Bearer ' + token}
 
 default_dag_args = {
     # The start_date describes when a DAG is valid / can be run. Set this to a
     # fixed point in time rather than dynamically, since it is evaluated every
     # time a DAG is parsed. See:
     # https://airflow.apache.org/faq.html#what-s-the-deal-with-start-date
-    'start_date': datetime.datetime(2020, 9, 17),
+    'start_date': datetime.datetime(2020, 9, 20),
     'retries': 2,
     'retry_delay': datetime.timedelta(minutes=5)
 }
@@ -52,35 +50,45 @@ with models.DAG(
     # BSQL script to pull the data from BigQuery
     bigquery_data_script = bigquery_operator.BigQueryOperator(
         task_id='bigquery_data_script',
-        bql=pathlib.Path(galileo.DAGS_DIR + "/bq_scripts_sandbox/bq_sql_ga_accounts_data_query.sql").read_text(), use_legacy_sql=False)
+        bql=pathlib.Path(constants.DAGS_DIR + "/bq_observatory_service/bq_sql_ga_accounts_data_query.sql").read_text(), use_legacy_sql=False)
 
     # BigQuery data fetch
     bigquery_data_fetch = bigquery_get_data.BigQueryGetDataOperator(
         task_id='bigquery_data_fetch',
-        dataset_id='dta_customers',
-        table_id='dta_ga_accounts'
-        # selected_fields=['domain_name', 'agency']
+        dataset_id= constants.DATASET_ID,
+        table_id= constants.TABLE_ID
         )
 
+
+    def prepare_data(data_, id_):
+        output= []
+        for datum in data_:
+            domain_name,	agency_name,	agency_dept,	hostname,	service_name,	webproperty_id = datum
+            logging.info(domain_name,	agency_name,	agency_dept,	hostname,	service_name,	webproperty_id)
+            output.append( {"service_name": service_name,
+                    "domain": domain_name, "ua_id": webproperty_id, "agency_id": id_} )
+        return output
+
+
+    def add_property(**context):
+        data_ops.add_agency(constants.AGENCY_DATA)
+        agency_id = data_ops.get_agencyId_by_name(constants.AGENCY_NAME)
+        xcom_data = context['ti'].xcom_pull(task_ids='bigquery_data_fetch')
+        data_out = prepare_data(xcom_data, agency_id)
+        response = requests.post(
+            constants.PROPERTIES_ENDPOINT, json=data_out, headers=header_token)
+        logging.info(response.json())
+
+
     # Write data to RDS
-    # post_data_to_rds = python_operator.PythonOperator(
-    #     task_id='post_data_to_rds',
-    #     python_callable=data_ops.add_data(token),
-    #     dag=dag
-    # )
-
-    def print_context(**context):
-        xcom_pull = context['ti'].xcom_pull(task_ids='bigquery_data_fetch')
-        logging.info('logging ' + json.dumps(xcom_pull))
-
-
-    t2 = python_operator.PythonOperator(
-        task_id='print_result',
-        python_callable=print_context,
+    post_data_to_rds = python_operator.PythonOperator(
+        task_id='post_data_to_rds',
+        python_callable=add_property,
         provide_context=True,
         op_kwargs=None,
         dag=dag,
     )
 
-bigquery_data_fetch >> t2
+    
+bigquery_data_fetch >> post_data_to_rds
 
