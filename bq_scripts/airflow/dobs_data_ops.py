728937616882d6f7a667dd262f46b32ec209c903
mufaddal <mufaddal.taiyab-ali@dta.gov.au>
2020-10-06 17:29:10 +1100

    New output method and error handling script update

--- bq_scripts/airflow/dobs_data_ops.py
+++ bq_scripts/airflow/dobs_data_ops.py
@@ -8,14 +8,13 @@ header_token = {'Authorization': 'Bearer ' + token}
 
 
 def add_agency(agencyData):
-    # data = [{
-    #     "name": "DTA Comms",
-    #     "emailHosts": ["@dta.gov.au", "@digital.gov.au"]
-    # }]
     response = requests.post(dobs_constants.AGENCIES_ENDPOINT,
                              json=agencyData, headers=header_token)
     responseData = response.json()
-    print(responseData)
+    if responseData['statusCode'] != '400':
+        logging.info('Error: ' + responseData['message'])
+    else:
+        logging.info("Data output written successfully: " + responseData)
 
 
 def get_agencyId_by_name(agency_name):
@@ -38,7 +37,10 @@ def add_property(propertyData):
     response = requests.post(
         dobs_constants.PROPERTIES_ENDPOINT, json=propertyData, headers=header_token)
     responseData = response.json()
-    print(responseData)
+    if responseData['statusCode'] != '400':
+        logging.info('Error: ' + responseData['message'])
+    else:
+        logging.info("Data output written successfully: " + responseData)
 
 
 def get_all_properties():
@@ -52,10 +54,20 @@ def get_all_properties():
 def get_property_by_uaid(ua_id):
     response = requests.get(f'{dobs_constants.PROPERTIES_ENDPOINT}/{ua_id}')
     responseData = response.json()
-    print(responseData)
+    logging.info(responseData)
     return responseData
 
 
 def delete_property_by_uaid(ua_id):
     del_res = requests.delete(dobs_constants.PROPERTIES_ENDPOINT+ '/' + ua_id, headers=header_token)
     logging.info(del_res.json())
+
+
+def add_output(outData, uaid):
+    response = requests.post(
+        dobs_constants.OUTPUTS_ENDPOINT + uaid, json=outData, headers=header_token)
+    responseData = response.json()
+    if responseData['statusCode'] != '400':
+        logging.info('Error: ' + responseData['message'])
+    else:
+        logging.info("Data output written successfully: " + responseData)
