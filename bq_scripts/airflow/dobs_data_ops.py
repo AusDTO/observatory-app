56eb701fa1886fbeee0e514fdce43ba240df9ff9
mufaddal <mufaddal.taiyab-ali@dta.gov.au>
2020-10-08 18:08:58 +1100

    Data operations script error handling updated

--- bq_scripts/airflow/dobs_data_ops.py
+++ bq_scripts/airflow/dobs_data_ops.py
@@ -2,6 +2,7 @@ import requests
 import dobs_signin
 import dobs_constants
 import logging
+import json
 
 token = dobs_signin.ACCESS_TOKEN
 header_token = {'Authorization': 'Bearer ' + token}
@@ -11,10 +12,10 @@ def add_agency(agencyData):
     response = requests.post(dobs_constants.AGENCIES_ENDPOINT,
                              json=agencyData, headers=header_token)
     responseData = response.json()
-    if responseData['statusCode'] != '400':
-        logging.info('Error: ' + responseData['message'])
+    if responseData['statusCode'] != 400:
+        logging.info('Error: ' + json.dumps(responseData))
     else:
-        logging.info("Data output written successfully: " + responseData)
+        logging.info("Data output written successfully: " + json.dumps(responseData))
 
 
 def get_agencyId_by_name(agency_name):
@@ -30,24 +31,24 @@ def get_all_agencies():
     response = requests.get(dobs_constants.AGENCIES_ENDPOINT,
                             headers=header_token)
     responseData = response.json()
-    print(responseData)
+    logging.info(json.dumps(responseData))
 
 
 def add_property(propertyData):
     response = requests.post(
         dobs_constants.PROPERTIES_ENDPOINT, json=propertyData, headers=header_token)
     responseData = response.json()
-    if responseData['statusCode'] != '400':
-        logging.info('Error: ' + responseData['message'])
+    if responseData['statusCode'] != 400:
+        logging.info('Error: ' + json.dumps(responseData))
     else:
-        logging.info("Data output written successfully: " + responseData)
+        logging.info("Data output written successfully: " + json.dumps(responseData))
 
 
 def get_all_properties():
     response = requests.get(dobs_constants.PROPERTIES_ENDPOINT,
                             headers=header_token)
     responseData = response.json()
-    print(responseData)
+    logging.info(json.dumps(responseData))
     return response.raise_for_status()
 
 
@@ -60,14 +61,24 @@ def get_property_by_uaid(ua_id):
 
 def delete_property_by_uaid(ua_id):
     del_res = requests.delete(dobs_constants.PROPERTIES_ENDPOINT+ '/' + ua_id, headers=header_token)
-    logging.info(del_res.json())
+    responseData = del_res.json()
+    logging.info(json.dumps(responseData))
 
 
 def add_output(outData, uaid):
     response = requests.post(
         dobs_constants.OUTPUTS_ENDPOINT + uaid, json=outData, headers=header_token)
     responseData = response.json()
-    if responseData['statusCode'] != '400':
-        logging.info('Error: ' + responseData['message'])
+    if responseData['statusCode'] != 400:
+        logging.info('Error: ' + json.dumps(responseData))
     else:
-        logging.info("Data output written successfully: " + responseData)
+        logging.info("Data output written successfully: " + json.dumps(responseData))
+
+
+def update_output(outData,ua_id, type_):
+    response = requests.put(dobs_constants.OUTPUTS_ENDPOINT + ua_id + '?type=' + type_ , json=outData, headers=header_token)
+    responseData = response.json()
+    if responseData['statusCode'] != 200:
+        logging.info('Error: ' + json.dumps(responseData))
+    else:
+        logging.info("Data output updated successfully: " + json.dumps(responseData))
